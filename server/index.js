4777280cc6356a3593f897a2035af43a0ed34d45
Vikesh-Mehta <vikeshmehta49@gmail.com>
2026-01-14 21:24:52 +0530

    Configure CORS for production

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 
@@ -8,7 +9,12 @@ const app = express();
 connectDB();
 
 // Init Middleware
-app.use(cors());
+const corsOptions = {
+  origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  credentials: true,
+  optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
 app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('API Running'));
